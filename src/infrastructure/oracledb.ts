3f2227711ce73101f042db0d30c15f3ca36d7d13
alvaro <cberna.alvaro@gmail.com>
2025-04-09 18:45:15 -0400

    -

--- src/infrastructure/oracledb.ts
+++ src/infrastructure/oracledb.ts
@@ -7,7 +7,7 @@ export class OracleDBService {
     cs = "tcps://adb.sa-santiago-1.oraclecloud.com:1522/" + envs.DB_SERVICE_NAME;
     connection = async () => {
         return await oracledb.getConnection({
-            user: envs.DB_USER,
+            user: 'admin',
             password: envs.DB_USER_PASSWORD,
             connectString: this.cs,
             configDir: __dirname + `../../../wallet_db`,
