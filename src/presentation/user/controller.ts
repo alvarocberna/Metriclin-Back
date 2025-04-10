423ba75377c69d4a992294c8a6da6ddf1408ffdc
alvaro <cberna.alvaro@gmail.com>
2025-04-09 23:03:04 -0400

    cierre de conexión añadido

--- src/presentation/user/controller.ts
+++ src/presentation/user/controller.ts
@@ -1,49 +1,59 @@
 import { Request, Response } from "express";
 import { OracleDBService } from "../../infrastructure/oracledb";
 
-// const usuarios = [
-//     { rut: '15123123', dv_rut: 'K', nombre: 'Pedro', ap_paterno: 'Pascal', ap_materno: 'Perez' },
-//     { rut: '16123123', dv_rut: 'K', nombre: 'Jorge', ap_paterno: 'Gonzalez', ap_materno: 'Silva' }
-// ]
-
 export class UserController {
 
     db = new OracleDBService();
-    connection = this.db.connection();
 
     constructor() { }
 
     public getUsers = async (req: Request, res: Response) => {
-        const connect = await this.connection;
-        const users = await connect.execute(`SELECT rut, nombre, ap_paterno, ap_materno, fecha_nac, correo, num_celular  
-            FROM USUARIO`);
-        if (users) {
-            res.json(users.rows);
-        } else {
-            res.status(404).json({ error: `No users found` });
+        let connection;
+        try {
+            connection = await this.db.connection();
+            const result = await connection.execute(`SELECT rut, nombre, ap_paterno, ap_materno, fecha_nac, correo, num_celular  
+                FROM USUARIO`);
+            res.json(result.rows);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Error en la consulta');
+        } finally {
+            if (connection) {
+                try {
+                    await connection.close();
+                    console.log('Conexión cerrada');
+                } catch (closeErr) {
+                    console.error('Error al cerrar la conexión:', closeErr);
+                }
+            }
+
         }
-        return;
+
     }
 
     public getUserByRut = async (req: Request, res: Response) => {
         const rut = req.params.rut;
-        const connect = await this.connection;
-        let user = [];
-
+        let connection;
         try {
-            user = await connect.execute(`SELECT rut, nombre, ap_paterno, ap_materno, fecha_nac, correo, num_celular  
+            connection = await this.db.connection();
+            const result = await connection.execute(`SELECT rut, nombre, ap_paterno, ap_materno, fecha_nac, correo, num_celular  
                 FROM USUARIO WHERE rut = ${rut}`);
+            res.json(result.rows);
         } catch (error) {
-            console.error('Error executing query:', error);
+            console.error(error);
+            res.status(500).send('Error en la consulta');
+        } finally {
+            if (connection) {
+                try {
+                    await connection.close();
+                    console.log('Conexión cerrada');
+                } catch (closeErr) {
+                    console.error('Error al cerrar la conexión:', closeErr);
+                }
+            }
+
         }
 
-        if (user.rows.length === 0) {
-            res.status(404).json({ error: `User with rut ${rut} not found` });
-            return;
-        } else {
-            res.json(user.rows[0])
-            return;
-        }
     }
 
 }
